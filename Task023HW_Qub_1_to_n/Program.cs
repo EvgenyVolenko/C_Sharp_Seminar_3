3310e2491b6c19b9ba9b48977e9580e2b5cf55eb
Evgeny <evolenko@inbox.ru>
2022-11-29 00:21:30 +0300

    Added Task023

---
+++ Task023HW_Qub_1_to_n/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+double qub;
+int count = 1;
+
+Console.Write("Введите число: ");
+int digit = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[digit];
+
+
+while (count <= digit)
+{
+    qub = Math.Pow(Convert.ToDouble(count), 3); // Можно конечно было просто count*coun*count. А так что-то новое.
+    Console.WriteLine($"Куб числа {count} = {qub}");
+    count++;
+}
