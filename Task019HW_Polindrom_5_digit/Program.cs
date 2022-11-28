f6a5edd22cdfe16aeb83cdb5a21e25d161b17868
Evgeny <evolenko@inbox.ru>
2022-11-28 22:42:59 +0300

    Added Task019

---
+++ Task019HW_Polindrom_5_digit/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число
+// и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Clear();
+Console.Write("Введите пятизначное число ");
+
+int digit, znak;
+int[] byDigit = new int[5];
+int index = 0;
+
+while (true)
+{
+    digit = Convert.ToInt32(Console.ReadLine());
+    if (digit > 9999 & digit < 100000) break;
+    else
+        Console.WriteLine("Ну на русском же написано... ПЯТИЗНАЧНОЕ ЧИСЛО!!! Еще попытка");
+}
+
+while (digit > 0)
+{
+    znak = digit % 10;
+    byDigit[index] = znak;
+    digit = digit / 10;
+    index++;
+}
+
+if (byDigit[0] == byDigit[4] && byDigit[1] == byDigit[3]) 
+{
+    Console.WriteLine("Введенное число ПАЛИНДРОМ!");
+}
+else 
+{
+    Console.WriteLine("Введенное число НЕ палиндром!");
+}
