0fd777b3838df1299fc8fb463d5f552e3e0f63b2
Evgeny <evolenko@inbox.ru>
2022-11-29 00:11:41 +0300

    Added Task022

---
+++ Task022CW_Quad_1_to_n_and_dop/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 22: Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+// Доп. задание
+// Написать метод для разворота массива, 
+// не используя память под дополнительный массив
+
+Console.Clear();
+int quad;
+int count = 1;
+
+Console.Write("Введите число: ");
+int digit = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[digit];
+
+
+while (count <= digit)
+{
+    quad = count * count;
+    Console.WriteLine($"Квадраты числа {count} = {quad}");
+    array[count - 1] = quad;
+    count++;
+}
+
+Console.WriteLine("Значения прямого массива: ");
+
+printArray(array);
+Console.WriteLine();
+razvorot(array);
+printArray(array);
+
+void razvorot (int[] arr)
+{
+    int temp = 0;
+    for (int i = 0; i <= arr.Length / 2; i++)
+    {
+        temp = arr[arr.Length - 1 - i];
+        arr[arr.Length - 1 - i] = arr[i];
+        arr[i] = temp;
+    }
+
+    Console.WriteLine("Значения развернутого массива:");
+}
+
+void printArray (int[] arr)
+{
+    foreach (int item in arr)
+    {
+        Console.Write(item + ", ");
+    }
+}
